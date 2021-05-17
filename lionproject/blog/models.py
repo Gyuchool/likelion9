cd0ff2a8881351756d8f8a287354b1f0187e80e2
Gyuchool <balance0591@naver.com>
2021-05-17 13:10:22 +0900

    lion_project

---
+++ lionproject/blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Blog(models.Model): #models class 내의 method사용
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField() #글자 제한 없음
+
