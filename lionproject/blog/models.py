57d4a8cc331e56f5631d923cfc775104dad725a6
Gyuchool <balance0591@naver.com>
2021-05-17 13:19:34 +0900

    model 생성

--- lionproject/blog/models.py
+++ lionproject/blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-class Blog(models.Model): #models class 내의 method사용
+class Blog(models.Model): #models class 내의 method사용, id column auto create
     title = models.CharField(max_length=200)
     writer = models.CharField(max_length=100)
     pub_date = models.DateTimeField()
     body = models.TextField() #글자 제한 없음
+
+    def __str__(self):
+        return self.title
 
