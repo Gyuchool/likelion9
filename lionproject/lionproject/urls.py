9a164588bfdce4deb3b195346017f359dc15d081
Gyuchool <balance0591@naver.com>
2021-05-22 13:10:06 +0900

    Create

--- lionproject/lionproject/urls.py
+++ lionproject/lionproject/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name="home"),
     path('<str:id>',detail,name="detail"), #매개변수로 들어가고 값에 따라 페이지 달라짐
+    path('new/',new,name="new"),
+    path('create/',create,name="create"),
 ]
