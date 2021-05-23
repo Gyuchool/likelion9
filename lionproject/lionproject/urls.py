61113e22ddec0eb7188a05a3f18f20e8a7eb7733
Gyuchool <balance0591@naver.com>
2021-05-23 11:52:53 +0900

    update

--- lionproject/lionproject/urls.py
+++ lionproject/lionproject/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('<str:id>',detail,name="detail"), #매개변수로 들어가고 값에 따라 페이지 달라짐
     path('new/',new,name="new"),
     path('create/',create,name="create"),
+    path('edit/<str:id>',edit,name="edit"),
+    path('update/<str:id>',update,name="update"),
 ]
