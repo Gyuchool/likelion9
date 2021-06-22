5e838dd3cc38012d7cad3b4cba379b9cc9b4b2b9
Gyuchool <balance0591@naver.com>
2021-06-22 23:31:11 +0900

    auth

--- lionproject/account/views.py
+++ lionproject/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 # Create your views here.
 
 def login_view(request):
@@ -13,7 +14,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
 
-            return redirect("home")
+        return redirect("home")
     else:
         form = AuthenticationForm()
         return render(request, 'login.html',{'form':form})
@@ -21,3 +22,14 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("home")
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        return redirect("home")
+    else:
+        form = RegisterForm()
+        return render(request, 'signup.html',{'form':form})
